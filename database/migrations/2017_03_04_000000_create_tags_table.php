5d9b1e5e394751310262129f080577c31ba0755c
imanghafoori <imanghafoori1@gmail.com>
2020-11-19 19:19:24 +0330

    increase limits of columns

--- database/migrations/2017_03_04_000000_create_tags_table.php
+++ database/migrations/2017_03_04_000000_create_tags_table.php
@@ -15,12 +15,12 @@ class CreateTagsTable extends Migration
     {
         Schema::create('temp_tags', function (Blueprint $table) {
             $table->unsignedBigInteger('id', true);
-            $table->string('taggable_type', 25);
+            $table->string('taggable_type', 32);
             $table->unsignedBigInteger('taggable_id');
             $table->index(['taggable_type', 'taggable_id']);
 
             $table->json('payload')->nullable();
-            $table->string('title', 30)->nullable();
+            $table->string('title', 32)->nullable();
             $table->timestamp('expired_at')->nullable()->index();
             $table->timestamp('created_at')->nullable();
 
